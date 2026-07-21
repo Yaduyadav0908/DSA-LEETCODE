02a95154ac39f72141854b2daa6bb869afd5803b
yadu yadav <yaduyadav0908@gmail.com>
2026-07-21 11:26:28 +0530

    Time: 2 ms (89.01%) | Memory: 48.1 MB (23.95%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int mx=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>mx){
+                return false;
+            }
+            mx=Math.max(mx,(i+nums[i]));
+            if(mx>=nums.length-1){
+                return true;
+            }
+
+        }
+        return true;
+    }
+}
